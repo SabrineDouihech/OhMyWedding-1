3b45535b18adc1f274398e7d347b6c80fd15c177
Mohamed Dhia <dhiaweslati16@gmail.com>
2021-08-31 17:52:37 +0100

    (fix) id auto icrement

--- OhMyWedding/models/Food.js
+++ OhMyWedding/models/Food.js
@@ -1,32 +1,39 @@
 module.exports = (sequelize, Sequelize) => {
-    const Food = sequelize.define('Food', {
-        id: {
-            type: Sequelize.INTEGER,
-            primaryKey: true
-        },
-        name: {
-            type: Sequelize.STRING
-        },
-        description: {
-            type: Sequelize.STRING
-        },
-        image: {
-            type: Sequelize.STRING
-        },
-        price: {
-            type: Sequelize.INTEGER
-        },
-        persons: {
-            type: Sequelize.INTEGER
-        },
-        foodtype: {
-            type: Sequelize.STRING
-        },
-        state: {
-            type: Sequelize.STRING
-        }
-    }, { // options
-        //     timestamps: false  	
-    });
-    return Food;
-}
+  const Food = sequelize.define(
+    "Food",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+      },
+      persons: {
+        type: Sequelize.INTEGER,
+      },
+      foodtype: {
+        type: Sequelize.STRING,
+      },
+      state: {
+        type: Sequelize.STRING,
+      },
+    },
+    {
+      // options
+      //     timestamps: false
+    }
+  );
+  return Food;
+};
--- OhMyWedding/models/Hosts.js
+++ OhMyWedding/models/Hosts.js
@@ -1,38 +1,40 @@
 module.exports = (sequelize, Sequelize) => {
-    const Hosts = sequelize.define(
-        "Hosts",
-        {
-            id: {
-                type: Sequelize.INTEGER,
-                primaryKey: true,
-            },
-            name: {
-                type: Sequelize.STRING,
-            },
-            image: {
-                type: Sequelize.STRING,
-            },
-            description: {
-                type: Sequelize.STRING,
-            },
-            price: {
-                type: Sequelize.INTEGER,
-            },
-            persons: {
-                type: Sequelize.INTEGER,
-            },
-            availabledate: {
-                type: Sequelize.DATE,
-            },
-            state: {
-                type: Sequelize.STRING,
-            },
-        },
-        {
-            // options
-            timestamps: false,
-        }
-    );
+  const Hosts = sequelize.define(
+    "Hosts",
+    {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.STRING,
+      },
+      price: {
+        type: Sequelize.INTEGER,
+      },
+      persons: {
+        type: Sequelize.INTEGER,
+      },
+      availabledate: {
+        type: Sequelize.DATE,
+      },
+      state: {
+        type: Sequelize.STRING,
+      },
+    },
+    {
+      // options
+      timestamps: false,
+    }
+  );
 
-    return Hosts;
+  return Hosts;
 };
