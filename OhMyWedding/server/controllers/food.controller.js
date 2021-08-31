eb5ea4087dbf8eec06d37bdf7fe11f7987943680
waeeelhajji <waelhajji.contact@gmail.com>
2021-08-31 20:48:29 +0200

    merging team work

--- OhMyWedding/server/controllers/food.controller.js
+++ OhMyWedding/server/controllers/food.controller.js
@@ -5,21 +5,34 @@ var db = require('../../config/db.confing');
 
 const getFood = async function (req, res) {
   try {
-    const packages = await db.food.findAll({});
-    res.status(200).send(packages);
+    const food = await db.food.findAll({});
+    res.status(200).send(food);
   }
   catch (error) {
     res.status(400).send(error);
   }
 };
-// const createFood = (input) => {
-//   const food = await db.food.create({
-//     id: re
-//   });
-// }
+
+const postFood = async function (req, res) {
+  try {
+    const createfoodi = await db.food.create({
+      id: req.id.id,
+      name: req.body.name,
+      image: req.body.image,
+      price: req.body.price,
+      persons: req.body.persons,
+      foodtype: req.body.foodtype,
+      state: req.body.state
+    })
+    res.status(200).send(createfoodi);
+  }
+  catch (error) {
+    res.status(400).send(error);
+  }
+}
 
 
 
 
 
-module.exports = { getFood };
+module.exports = { getFood, postFood };
