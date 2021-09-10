e3c4abe6a7a48e83fec3c186271728e4bb6c2bab
waeeelhajji <waelhajji.contact@gmail.com>
2021-09-11 00:27:44 +0200

    complete with adding the pachages in frontend and the backend

--- OhMyWedding/src/app/invitationcard.service.ts
+++ OhMyWedding/src/app/invitationcard.service.ts
@@ -8,20 +8,23 @@ export class InvitationCardsService {
   constructor(private http: HttpClient) {}
 
   getSomeInvitationCards() {
-    return this.http.get('http://localhost:3000/api/invitationcards');
+    return this.http.get<any[]>('http://localhost:3000/api/invitationcards');
   }
 
-postCards(data : any){
-    return this.http.post('http://localhost:3000/api/invitationcards',data);
-}
-deleteCards(id:string){
-    return this.http.delete(`http://localhost:3000/api/invitationcards/${id}`)
-}
+  postCards(data: any) {
+    return this.http.post('http://localhost:3000/api/invitationcards', data);
+  }
+  deleteCards(id: string) {
+    return this.http.delete(`http://localhost:3000/api/invitationcards/${id}`);
+  }
 
-uptCards(id:string,data:any){
-    return this.http.put(`http://localhost:3000/api/invitationcards/${id}`,data)
-}
-uploadImg(img : any) {
-    return this.http.post("http://localhost:3000/upload", img);
+  uptCards(id: string, data: any) {
+    return this.http.put(
+      `http://localhost:3000/api/invitationcards/${id}`,
+      data
+    );
+  }
+  uploadImg(img: any) {
+    return this.http.post('http://localhost:3000/upload', img);
   }
 }
--- OhMyWedding/src/app/musicalband.service.ts
+++ OhMyWedding/src/app/musicalband.service.ts
@@ -2,26 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MusicalbandService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
   getABand() {
-    return this.http.get('http://localhost:3000/api/musicalband');
+    return this.http.get<any[]>('http://localhost:3000/api/musicalband');
   }
-  postABand(data : any){
-    return this.http.post('http://localhost:3000/api/musicalband',data);
-}
-deleteABand(id:string){
-    return this.http.delete(`http://localhost:3000/api/musicalband/${id}`)
-}
+  postABand(data: any) {
+    return this.http.post('http://localhost:3000/api/musicalband', data);
+  }
+  deleteABand(id: string) {
+    return this.http.delete(`http://localhost:3000/api/musicalband/${id}`);
+  }
 
-uptABand(id:string,data:any){
-    return this.http.put(`http://localhost:3000/api/musicalband/${id}`,data)
-}
-uploadImg(img : any) {
-    return this.http.post("http://localhost:3000/upload", img);
+  uptABand(id: string, data: any) {
+    return this.http.put(`http://localhost:3000/api/musicalband/${id}`, data);
+  }
+  uploadImg(img: any) {
+    return this.http.post('http://localhost:3000/upload', img);
   }
 }
-
